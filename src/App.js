71395f2787e33a5269e6f1ef360b8242a77a82dc
nguyendong <nguyendong220496@gmail.com>
2023-11-17 08:11:35 +0700

    trang ca nhan ProfileLover

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import {Route, Routes} from "react-router";
 import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
-
+import {HomeProfileLover} from "./pages/HomeProfileLover/HomeProfileLover";
 function App() {
   return (
       <Routes>
       <Route path={"/"} element={<Layout/>}>
         <Route path={""} element={<Home/>}></Route>
-        {/*<Route path={"register"} element={<Register/>}></Route>*/}
+        <Route path={"/homeProfileLover"} element={<HomeProfileLover/>}>
+
+        </Route>
         {/*<Route path={"registerCCDV/:id"} element={<RegisterCCDV/>}></Route>*/}
         {/*<Route path={"registerProfile"} element={<RegisterProfile/>}></Route>*/}
         {/*<Route path={"/userDetail/:id"} element={<Detail/>}></Route>*/}
