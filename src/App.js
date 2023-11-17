75c87b3daa09b556cea4aa783d5f4141d1614a48
Nguyenanhtuan1997 <nguyenanhtuan041297@gmail.com>
2023-11-17 08:22:00 +0700

    trang chi tiet lover

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import {Route, Routes} from "react-router";
 import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
+import {InfoLover} from "./pages/InfoLover/InfoLover";
+
 
 function App() {
   return (
       <Routes>
       <Route path={"/"} element={<Layout/>}>
         <Route path={""} element={<Home/>}></Route>
+          <Route path={'/info-lover/:id'} element={<InfoLover/>}></Route>
         {/*<Route path={"register"} element={<Register/>}></Route>*/}
         {/*<Route path={"registerCCDV/:id"} element={<RegisterCCDV/>}></Route>*/}
         {/*<Route path={"registerProfile"} element={<RegisterProfile/>}></Route>*/}
