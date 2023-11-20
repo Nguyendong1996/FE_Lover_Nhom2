4b7bcb3869cec7ceeafd029a4b2c2b457beec727
trongtai <trongtai.nguyen789@gmail.com>
2023-11-20 21:26:38 +0700

    them toast

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,15 +3,16 @@ import Header from "./Header";
 import {Outlet} from "react-router";
 import Footer from "./Footer";
 import {AppProvider} from "../context/AppContext";
+import {ToastContainer} from "react-toastify";
 
 const Layout = () => {
     return (
         <>
-            <AppProvider>
-                <Header/>
-                <Outlet/>
-                <Footer/>
-            </AppProvider>
+                <AppProvider>
+                    <Header/>
+                    <Outlet/>
+                    <Footer/>
+                </AppProvider>
         </>
     );
 };
